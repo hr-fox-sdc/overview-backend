d9e94be4059156e435e09dda9b2b8394d20fc71a
Huong <htnguuyen@gmail.com>
2022-11-07 23:40:49 -0500

    Added loader key & endpoint

--- overview-backend-server/index.js
+++ overview-backend-server/index.js
@@ -1,18 +1,26 @@
 const express = require('express');
+const fs = require('fs');
 const connectionPool = require('./connect.js')
 // const bodyParser = require('body-parser');
 const db = require('./queries.js');
 const app = express();
 const port = 3005;
-// const cors = require('cors');
+require('dotenv').config();
+const path = require('path');
+const cors = require('cors');
 
+// const reactConfig = require(path.join(__dirname, '../config/config.static.json'))[env];
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
+
+// app.use(express.static(path.join(__dirname, reactConfig))); // serving react files
 
 // app.get('/', (request, response) => {
 //   response.json({ info: 'Node.js, Express, and Postgres API' });
 // });
+
+const loaderKey = fs.readFileSync('./loaderio-a402317815c58b54728bac203314b3ff.txt').toString();
 
 connectionPool.connect((err, client, release) => {
   if (err) {
@@ -21,11 +29,18 @@ connectionPool.connect((err, client, release) => {
   console.log('connected to database')
 })
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // overview routes
 app.get('/products', db.getAllProducts);
 app.get('/products/:product_id', db.getProduct);
 app.get('/products/:product_id/styles', db.getProductStyles);
 app.get('/products/:product_id/related', db.getRelatedItems);
+app.get('/loaderio-a402317815c58b54728bac203314b3ff.txt', (req, res) => res.send(loaderKey))
 // app.post('/cart', addToCart);
 
 app.listen(port, () => {
