86d532cf9b2b6e9aee22f9ba03d14aaf8900d790
Huong <htnguuyen@gmail.com>
2022-11-10 19:07:03 -0500

    File clean up

--- overview-backend-server/index.js
+++ overview-backend-server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const fs = require('fs');
 const connectionPool = require('./connect.js')
-// const bodyParser = require('body-parser');
 const db = require('./queries.js');
 const app = express();
 const port = 3005;
@@ -9,16 +8,8 @@ require('dotenv').config();
 const path = require('path');
 const cors = require('cors');
 
-// const reactConfig = require(path.join(__dirname, '../config/config.static.json'))[env];
-
 app.use(cors());
 app.use(express.json());
-
-// app.use(express.static(path.join(__dirname, reactConfig))); // serving react files
-
-// app.get('/', (request, response) => {
-//   response.json({ info: 'Node.js, Express, and Postgres API' });
-// });
 
 const loaderKey = fs.readFileSync('./loaderio-a402317815c58b54728bac203314b3ff.txt').toString();
 
@@ -40,7 +31,7 @@ app.get('/products', db.getAllProducts);
 app.get('/products/:product_id', db.getProduct);
 app.get('/products/:product_id/styles', db.getProductStyles);
 app.get('/products/:product_id/related', db.getRelatedItems);
-app.get('/loaderio-a402317815c58b54728bac203314b3ff.txt', (req, res) => res.send(loaderKey))
+app.get('/loaderio-a402317815c58b54728bac203314b3ff.txt', (req, res) => res.send(loaderKey));
 // app.post('/cart', addToCart);
 
 app.listen(port, () => {
